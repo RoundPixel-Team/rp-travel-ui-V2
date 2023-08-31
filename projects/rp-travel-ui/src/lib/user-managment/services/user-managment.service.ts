2ab0c1e588717396636307fdaca9066144121152
Maysara-Nasser <85829180+Maysara-Nasser@users.noreply.github.com>
2023-08-31 15:37:07 +0300

    update in user managment

--- projects/rp-travel-ui/src/lib/user-managment/services/user-managment.service.ts
+++ projects/rp-travel-ui/src/lib/user-managment/services/user-managment.service.ts
@@ -1,8 +1,9 @@
-import { Inject, Injectable } from '@angular/core';
+import { Injectable,inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
 import { UserManagmentApiService } from './user-managment-api.service';
 import { Router } from '@angular/router';
+import { userModel } from '../interfaces';
 
 const REG_DATA = 'reg_data';
 @Injectable({
@@ -11,9 +12,14 @@ const REG_DATA = 'reg_data';
 export class UserManagmentService {
   //#region Variablses
   subscription: Subscription = new Subscription();
-  api = Inject(UserManagmentApiService) 
+  api = inject(UserManagmentApiService) 
   loginForm: FormGroup = new FormGroup({});
   registerForm: FormGroup = new FormGroup({});
+
+  loading : boolean = false
+
+  currentUser! : userModel ;
+  userChange : Subject<string> = new Subject
   //#endregion
 
   constructor(private __fb: FormBuilder,private __router: Router) {}
@@ -31,24 +37,19 @@ export class UserManagmentService {
    */
   initRegisterForm() {
     this.registerForm = this.__fb.group( {
-      username: ['',  [Validators.required, Validators.pattern('([a-zA-Z]+ +[a-zA-Z]*)*')],],
-      mobile: ['', [Validators.required]],
-      mail: [
-        '',
-        [
-          Validators.required,
-          Validators.email,
-          Validators.pattern(
-            '^[a-z0-9A-Z._%+-]+@[a-z0-9A-Z.-]+\\.[a-zA-Z]{2,4}$'
-          ),
-        ],
-      ],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['', [Validators.required]],
-      checkBox: [, [Validators.requiredTrue]],
+    Isbase:new FormControl(1),
+    Email: new FormControl('',[Validators.email,Validators.minLength(8),Validators.required]),
+    Password: new FormControl('',[Validators.required,Validators.minLength(8)]),
+    FirstName: new FormControl(''),
+    LastName: new FormControl(''),
+    ImageURL: new FormControl(''),
+    PhoneNumber: new FormControl('',[Validators.required,Validators.minLength(5)]),
+    ConfirmPassword: new FormControl('',[Validators.required,Validators.minLength(8)]),
+    User_Name: new FormControl (''),
+    notification:new FormControl(true)
     },
     {
-      validators: this.ConfirmPasswordValidator('password', 'confirmPassword'),
+      validators: this.ConfirmPasswordValidator('Password', 'ConfirmPassword'),
     });
   }
   /**
@@ -71,16 +72,22 @@ export class UserManagmentService {
    * @params router navigation name to navigate to another page (HOME PAGE) after login
    */
   loginSubmit(routerName:string){
+    this.loading = true
     if(this.loginForm.invalid){
       this.loginForm.markAllAsTouched()
+      this.loading = false
     }
     else {
       this.subscription.add(
-        this.api.login(this.loginForm.value).subscribe((val:any)=>{
-          console.log(val);
-          this.__router.navigate([`/${routerName}`])     //navigate to paramter name page after login
-          window.location.reload();
-        })
+        this.api.login(this.loginForm.value).subscribe((val:userModel)=>{
+          console.log("show me login submit",val);
+          this.loading = false
+          this.currentUser = val
+          this.userChange.next('logedin')
+          // this.__router.navigate([`/${routerName}`])     //navigate to paramter name page after login
+          // window.location.reload();
+        },
+        (err:any)=>{console.log("user login error",err);this.loading = false})
       );
     }
   }
@@ -89,15 +96,22 @@ export class UserManagmentService {
    * @params router navigation name to navigate to another page (LOGIN PAGE) after register
   */
   regitserSubmit(routerName:string){
+    this.loading = true
     if(this.registerForm.invalid){
       this.registerForm.markAllAsTouched();
+      this.loading = false
     }
     else{
-      this.api(this.registerForm.value).subscribe((val:any)=>{
+      this.api.signup(this.registerForm.value).subscribe((val:any)=>{
+        console.log("show me register submit",val)
         localStorage.setItem(REG_DATA,JSON.stringify(this.registerForm.value))
-        this.__router.navigate([`/${routerName}`]) //navigate to paramter name page after registerate the account
+        this.loading = false
+        this.currentUser = val
+        this.userChange.next('signedup')
+        // this.__router.navigate([`/${routerName}`]) //navigate to paramter name page after registerate the account
       },(error:any)=>{
-        console.log(error);
+        console.log("show me signup error",error);
+        this.loading = false
       })
     }
   }
