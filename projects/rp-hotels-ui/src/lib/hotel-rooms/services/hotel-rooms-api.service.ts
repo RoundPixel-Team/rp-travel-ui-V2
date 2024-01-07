36cfb3603a83e775156206c5360436400645b21b
NourhanSalah97 <norahansalah1997@gmail.com>
2024-01-07 22:18:57 +0200

    first commit

--- projects/rp-hotels-ui/src/lib/hotel-rooms/services/hotel-rooms-api.service.ts
+++ projects/rp-hotels-ui/src/lib/hotel-rooms/services/hotel-rooms-api.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { EnvironmentService } from '../../shared/services/environment.service';
+import { hotelRoomsResponse } from '../interfaces';
+import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +13,13 @@ export class HotelRoomsApiService {
   public env = inject(EnvironmentService)
 
   constructor() { }
+  
+  getHotelsRoomsApi(sid: string,hotelid:string,Pid: string){
+    let url =  this.env.Apihotels + "api/GetPackages?sid=" +sid +"&hotel=" +hotelid +"&Pid=" + Pid
+    return this.http.get<hotelRoomsResponse>(url).pipe(take(1));
+  }
+  GetRoomCancelPolicy(sid: string, hotelcode: any, roomindex: any,packageKey:string, PId: any) {
+    let url = `${this.env.Apihotels}/api/getcancelpolicy?sid=${sid}&hotelcode=${hotelcode}&roomindex=${roomindex}&packageKey=${packageKey}&PId=${PId}`;
+    return this.http.get<any[]>(url).pipe(take(2));
+  }
 }
--- projects/rp-hotels-ui/src/lib/hotel-rooms/services/hotel-rooms.service.ts
+++ projects/rp-hotels-ui/src/lib/hotel-rooms/services/hotel-rooms.service.ts
@@ -1,19 +1,34 @@
 import { Injectable, inject } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { HotelRoomsApiService } from './hotel-rooms-api.service';
+import { hotelRoomsResponse, roomCancelPolicy } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelRoomsService {
-
+roomsLoader:boolean = false;
+roomsData!:hotelRoomsResponse[];
+cancelPolicy!:roomCancelPolicy[];
   api = inject(HotelRoomsApiService)
 
   subscription : Subscription = new Subscription()
 
   constructor() { }
-
-
+getRooms(sid: string,hotelid:string,Pid: string){
+  this.roomsLoader=true;
+  this.api.getHotelsRoomsApi(sid,hotelid,Pid).subscribe((data) =>{
+    this.roomsLoader=false;
+    // this.roomsData=data;
+  }
+  
+    )
+}
+getCancelPolicy(sid: string, hotelcode: any, roomindex: any,packageKey:string, PId: any){
+this.api.GetRoomCancelPolicy(sid, hotelcode, roomindex,packageKey, PId).subscribe((data)=>{
+  this.cancelPolicy=data;
+});
+}
   /**
    * this function is responsible to destory any opened subscription on this service
    */
