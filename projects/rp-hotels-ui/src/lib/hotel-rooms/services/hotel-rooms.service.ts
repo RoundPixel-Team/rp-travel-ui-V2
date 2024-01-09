c9c03d4a954d1ed36b6246943ea267f0e0204b28
Nourhan-RoundPixel <Nourhan@round-pixel.net>
2024-01-09 16:31:34 +0200

    edit declartion

--- projects/rp-hotels-ui/src/lib/hotel-rooms/services/hotel-rooms.service.ts
+++ projects/rp-hotels-ui/src/lib/hotel-rooms/services/hotel-rooms.service.ts
@@ -9,7 +9,7 @@ import { hotelRoomsResponse, roomCancelPolicy } from '../interfaces';
 export class HotelRoomsService {
 roomsLoader:boolean = false;
 cancelLoader:boolean = false;
-roomsData!:hotelRoomsResponse;
+roomsData:hotelRoomsResponse=undefined!;
 cancelPolicy!:roomCancelPolicy[];
   api = inject(HotelRoomsApiService)
 
@@ -29,7 +29,9 @@ getRooms(sid: string,hotelid:string,Pid: string){
       if(data){
         this.roomsLoader=false;
       this.roomsData=data;
+      console.log(this.roomsData,'test Data');
       const groupedRooms = this.groupRooms(this.roomsData);
+      console.log(groupedRooms,'test grouping');
       }
       
     },(err:any)=>{
@@ -76,6 +78,7 @@ groupRooms(Roomsdata:hotelRoomsResponse){
         this.cancelPolicy=data;
         this.cancelLoader = false;
 
+        console.log(this.cancelPolicy,'test cancel policy');
 
       }
     },(err:any)=>{
