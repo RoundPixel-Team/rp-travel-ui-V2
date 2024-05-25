9818e45910eaa3a58846e38d23d89c83ded317f6
NourhanSalah97 <norahansalah1997@gmail.com>
2024-05-25 19:01:19 +0300

    add hotel confirmation services

--- projects/rp-hotels-ui/src/lib/hotel-confirmation/services/hotel-confirmation-api.service.ts
+++ projects/rp-hotels-ui/src/lib/hotel-confirmation/services/hotel-confirmation-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { EnvironmentService } from '../../shared/services/environment.service';
+import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +12,38 @@ export class HotelConfirmationApiService {
   public env = inject(EnvironmentService)
 
   constructor() { }
+  /**
+   * 
+   * @param url 
+   * @returns  the payment result status 
+   */
+  getHotelsPaymentResult(url: string) {
+    // get the payment result status for hotels
+    let api = `${this.env.prepay}api/paymentresult?${url}`;
+    return this.http.get<any>(api).pipe(take(1));
+  }
+  /**
+   *@param HGNu
+   @param searchid
+   * @param tok 
+   * @param url 
+   * @returns status after successful payment
+   */
+  HotelsPostProcessing(HGNu: string, searchid: string, tok: string, url: string) {
+    //  get satus after succesful payment
+    let api = `${this.env.Apihotels}/Api/ConfirmHotelStatus?sid=${searchid}&bookingNum=${HGNu}&tok=${tok}`;
+    console.log(api);
+    return this.http.get<any>(api).pipe(take(1));
+  }
+   /**
+   * 
+   * @param HGNu 
+   * @param searchid 
+   * @returns flight confirmation details after payment has been finshed
+   */
+  getHotelsConfirmation(HGNu: string, searchid: string) {
+    // get return conformtion from the clint
+    let api = `${this.env.Apihotels}/Api/Confirmation?sid=${searchid}&bookingNum=${HGNu}`;
+    return this.http.get<any>(api).pipe(take(3));
+  }
 }
--- projects/rp-hotels-ui/src/lib/hotel-confirmation/services/hotel-confirmation.service.ts
+++ projects/rp-hotels-ui/src/lib/hotel-confirmation/services/hotel-confirmation.service.ts
@@ -1,16 +1,40 @@
 import { Injectable, inject } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { hotelBookingModel } from '../interfces';
+import { HotelConfirmationApiService } from './hotel-confirmation-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelConfirmationService {
 
-  api = inject(HotelConfirmationService)
+  api = inject(HotelConfirmationApiService)
   subscription : Subscription = new Subscription()
-
+  loading : boolean = false
+  confirmationData! :hotelBookingModel  
+  error:any
   constructor() { }
 
+/**
+   * fetching confirmation data
+   * update state of [confirmationData:FlightSearchResult] in case of success response
+   * update loading state
+   * @param searchId 
+   * @param hgNum 
+   */
+getConfirmationData(searchId:string,hgNum:string){
+  this.loading = true
+  this.error = undefined
+  this.api.getHotelsConfirmation(hgNum,searchId).subscribe((res)=>{
+    if(res){
+      this.confirmationData = res
+      this.loading = false
+    }
+  },(err)=>{
+    this.error = err
+    this.loading = false
+  })
+}
 
   /**
    * this function is responsible to destory any opened subscription on this service
