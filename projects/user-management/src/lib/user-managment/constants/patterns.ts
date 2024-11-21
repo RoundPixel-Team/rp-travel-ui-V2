5595f3c0e1964eb43d220bcb30c4b326aa4df13d
WaleedAlmenawyRP <walid@round-pixel.net>
2024-11-21 11:41:19 +0200

    update: delete unused library (user-management)

--- projects/user-management/src/lib/user-managment/constants/patterns.ts
+++
@@ -1,5 +0,0 @@
-export const EMAIL_PATTERN = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)$/;
-
-export const PASSWORD_PATTERN = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[A-Za-z\d\W]{8,}$/
-
-export const PHONE_PATTERN = /^(?:\+?(\d{1,3})[-. ]?)?\d{10}$/;
