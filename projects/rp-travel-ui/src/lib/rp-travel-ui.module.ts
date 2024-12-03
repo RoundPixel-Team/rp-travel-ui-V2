bb64a51b03b5c3b2a229fda827ed4ffba8723a7d
Nourhan-RoundPixel <Nourhan@round-pixel.net>
2024-12-03 16:15:03 +0200

    add airport filter pipe

--- projects/rp-travel-ui/src/lib/rp-travel-ui.module.ts
+++ projects/rp-travel-ui/src/lib/rp-travel-ui.module.ts
@@ -12,6 +12,7 @@ import { HourMinutePipe } from './shared/pipes/hour-minute.pipe';
 import { LimitToPipe } from './shared/pipes/limit-to.pipe';
 import { ExchangePipe } from './shared/pipes/exchange.pipe';
 import { DatePipe } from '@angular/common';
+import { FilterAirportPipe } from './shared/pipes/filter-airport.pipe';
 
 
 
@@ -26,7 +27,8 @@ import { DatePipe } from '@angular/common';
     HotelecitesPipe,
     HourMinutePipe,
     LimitToPipe,
-    ExchangePipe
+    ExchangePipe,
+    FilterAirportPipe
   ],
   imports: [
     HttpClientModule,
@@ -47,7 +49,8 @@ import { DatePipe } from '@angular/common';
     HotelecitesPipe,
     HourMinutePipe,
     LimitToPipe,
-    ExchangePipe
+    ExchangePipe,
+    FilterAirportPipe
   ],
   providers:[HttpClient, DatePipe]
 })
---
+++ projects/rp-travel-ui/src/lib/shared/pipes/filter-airport.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterAirportPipe } from './filter-airport.pipe';
+
+describe('FilterAirportPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterAirportPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
