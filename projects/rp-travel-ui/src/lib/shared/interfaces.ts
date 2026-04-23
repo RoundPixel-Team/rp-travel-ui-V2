7025f57ba63515e4e528df17ce0d937dc05513b8
alimagdi12 <ali.alafani@round-pixel.net>
2026-04-23 13:32:33 +0200

    linting

--- projects/rp-travel-ui/src/lib/shared/interfaces.ts
+++ projects/rp-travel-ui/src/lib/shared/interfaces.ts
@@ -1,29 +1,30 @@
-export interface enviromentModel{
-    offlineSeats : string
-    searchflow   : string
-    BookingFlow  : string
-    FareRules    : string
-    asm          : string
-    Apihotels    : string
-    prepay  : string
-    users        : string
-    admin        : string
-    getDPayment  : string
-    bookHotels   : string
-    backOffice   : string
-    FlightTop    : string
-    staticPages?  : string
-    offers:{
-      getAll     : string,
-      getByID    : string,
-      BookOffer  : string,
-      RetriveItineraryDetails : string
-    }
+export interface enviromentModel {
+  offlineSeats: string;
+  searchflow: string;
+  BookingFlow: string;
+  salesRules?: string;
+  FareRules: string;
+  asm: string;
+  Apihotels: string;
+  prepay: string;
+  users: string;
+  admin: string;
+  getDPayment: string;
+  bookHotels: string;
+  backOffice: string;
+  FlightTop: string;
+  staticPages?: string;
+  offers: {
+    getAll: string;
+    getByID: string;
+    BookOffer: string;
+    RetriveItineraryDetails: string;
+  };
 }
 /**
  * used to save error messages with two language (arabic, english)
  */
-export interface AlertMsgModel{
-  arMsg:string | null | undefined
-  enMsg:string | null | undefined
+export interface AlertMsgModel {
+  arMsg: string | null | undefined;
+  enMsg: string | null | undefined;
 }
